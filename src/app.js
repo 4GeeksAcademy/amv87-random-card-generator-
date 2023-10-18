c892a06e0b05ba267af582cd3035b55a06466ba2
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-10-18 21:15:36 +0000

    added function

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
 };
+
+function randomCard() {
+  const cardNumber = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const cardSuit = ["♦", "♥", "♠", "♣"];
+  document.getElementById("randomNumber").innerHTML =
+    cardNumber[Math.floor(Math.random() * cardNumber.length)];
+  let suit = cardSuit[Math.floor(Math.random() * cardSuit.length)];
+  document.getElementById("randomSuit").innerHTML = suit;
+  if (suit == "♥" || suit == "♦") {
+    return (document.getElementById("randomSuit").style.color = "red");
+  } else {
+    return (document.getElementById("randomSuit").style.color = "black");
+  }
+}
