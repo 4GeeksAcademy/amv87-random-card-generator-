2878eed122300816c80e0aaebc556cc54beb4860
Álvaro M. Villar <144016805+amv87@users.noreply.github.com>
2023-10-24 20:29:37 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,32 +8,24 @@ window.onload = function() {
 function randomCard() {
   const cardNumber = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   const cardSuit = ["♦", "♥", "♠", "♣"];
-  document.getElementById("randomNumber").innerHTML =
-    cardNumber[Math.floor(Math.random() * cardNumber.length)];
+  document.getElementById("randomNumber").innerHTML = cardNumber[Math.floor(Math.random() * cardNumber.length)];
   let suit = cardSuit[Math.floor(Math.random() * cardSuit.length)];
   document.getElementById("randomSuitTop").innerHTML = suit;
   document.getElementById("randomSuitBottom").innerHTML = suit;
   if (suit == "♥" || suit == "♦") {
-    return [
-      (document.getElementById("randomSuitTop").style.color = "red"),
-      (document.getElementById("randomSuitBottom").style.color = "red")
-    ];
+    return [(document.getElementById("randomSuitTop").style.color = "red"), (document.getElementById("randomSuitBottom").style.color = "red")];
   } else {
-    return [
-      (document.getElementById("randomSuitTop").style.color = "black"),
-      (document.getElementById("randomSuitBottom").style.color = "black")
-    ];
+    return [(document.getElementById("randomSuitTop").style.color = "black"), (document.getElementById("randomSuitBottom").style.color = "black")];
   }
 }
 
 window.createRandom = function() {
   randomCard();
 };
+
 window.changeSize = function() {
-  document.getElementById("cardWhole").style.width =
-    document.getElementById("cardWidth").value + "px";
-  document.getElementById("cardWhole").style.height =
-    document.getElementById("cardHeight").value + "px";
+  document.getElementById("cardWhole").style.width = document.getElementById("cardWidth").value + "px";
+  document.getElementById("cardWhole").style.height = document.getElementById("cardHeight").value + "px";
 };
 
 setInterval(randomCard, 10000);
